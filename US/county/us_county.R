b3d8431a455be368705e02bdbd0253396e762a3f
Roger Fischer <roger.fischer@kaywa.com>
2016-01-07 12:34:19 -0800

    added 2011 and 2012

--- US/county/us_county.R
+++ US/county/us_county.R
@@ -12,11 +12,13 @@ dateDownloaded # "Thu Jan  7 10:35:21 2016"
 
 data = read.delim("data.txt", 
                sep=",",
+               header = FALSE,
                colClasses = "character",
                col.names=c("state_code", "state_fips", "county_fips", "county_name", "class_fips"))
 
 ## head(data)
 ## str(data)
+## dim(data)
 
 ## Export as csv
 ## data <- write.table(data, file="data.csv",sep=",",col.names=FALSE,row.names=FALSE)
@@ -26,4 +28,26 @@ data$fips <- paste(data$state_fips, data$county_fips, sep="")
 data <- data[c("id", "fips", "county_name", "state_code", "state_fips", "county_fips", "class_fips")]
 
 ## Export as csv
-## data <- write.table(data, file="us_county_2010.csv",sep=",",col.names=FALSE,row.names=FALSE)
+## write.table(data, file="us_county_2010.csv",sep=",",col.names=FALSE,row.names=FALSE)
+
+## Only States and DC (without Puerto Rico, Guam etc.) for 2010
+state_code <- unique(data$state_code)
+nonstate_code <- c("AS", "GU", "MP", "PR", "UM", "VI")
+
+library(dplyr)
+state_data <- slice(data, 1:3143)
+## Crosscheck with data[3144, ] => gives AS, fips 60010
+
+## Export state&DC data 2010
+## write.table(state_data, file="us_county_congressionalvote_2010.csv", sep="," ,col.names=TRUE, row.names=FALSE)
+
+## Only States and DC (without Puerto Rico, Guam etc.) for 2011, 2012
+## LaSalle Parish, Louisiana (22-059)
+## Name corrected from La Salle Parish (removing space) reported as of January 1, 2011.
+lasalle <- state_data[state_data$fips == "22059", ] 
+lasalle$county_name <- "LaSalle Parish"
+state_data[state_data$fips == "22059", ] <- lasalle
+
+## Export state&DC data 2011,2012
+write.table(state_data, file="us_county_congressionalvote_2011.csv", sep="," ,col.names=TRUE, row.names=FALSE)
+write.table(state_data, file="us_county_congressionalvote_2012.csv", sep="," ,col.names=TRUE, row.names=FALSE)
