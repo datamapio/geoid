21a896ec671eb5351e06b4e4fae52493f733f7fa
Roger Fischer <roger.fischer@kaywa.com>
2016-01-12 12:54:51 -0800

    changed the iowa reference

--- US/example/match_geoid_data_with_ext_data.R
+++ US/example/match_geoid_data_with_ext_data.R
@@ -1,8 +1,8 @@
 ## setwd("/Users/rogerfischer/datamap/geoid/US/example")
 ## getwd()
 
-## How to merge external result data with the geoid reference data
-##≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠
+## How to merge external result data (EXT) with the geoid reference data (REF)
+## ===========================================================================
 
 ## External Data: 2012 Data from the Guardian
 ## https://docs.google.com/spreadsheets/d/15_KGSulg72cU2Km8G3ZrZdLSctg_WSoGKiFCgihpTVs/edit#gid=21
@@ -11,29 +11,29 @@
 fileUrl <- "https://raw.githubusercontent.com/datamapio/geoid/master/US/example/primary_iowa_county_2012_ext.csv"
 download.file(fileUrl, destfile = "ext.csv", method = "curl")
 
-# Geoid reference Data for 2012
-# https://github.com/datamapio/geoid/blob/master/US/county/by_state/iowa/iowa_2011-2012.csv
-fileUrl <- "https://raw.githubusercontent.com/datamapio/geoid/master/US/county/by_state/iowa/iowa_county_2011-2012.csv"
-download.file(fileUrl, destfile = "geoid.csv", method = "curl")
+## The REF for 2012
+## https://github.com/datamapio/geoid/blob/master/US/county/by_state/iowa/iowa_county_2010-2015.csv
+fileUrl <- "https://raw.githubusercontent.com/datamapio/geoid/master/US/county/by_state/iowa/iowa_county_2010-2015.csv"
+download.file(fileUrl, destfile = "ref.csv", method = "curl")
 list.files()
 
 dateDownloaded <- date()
 dateDownloaded # "Mon Jan 11 11:14:22 2016"
 
 ext <- read.csv("ext.csv", header = TRUE, sep = ",", stringsAsFactors=FALSE)
-geoid <- read.csv("geoid.csv", header = TRUE, sep = ",", stringsAsFactors=FALSE, 
+ref <- read.csv("ref.csv", header = TRUE, sep = ",", stringsAsFactors=FALSE, 
                   colClasses = c("character", "character", "character", "character", 
                                  "character", "character", "character"))
 
 ## head(ext)
-## head(geoid)
+## head(ref)
 ## str(ext)
-## str(geoid)
+## str(ref)
 ## dim(ext)
-## dim(geoid)
+## dim(ref)
 
 # Merge by fips
-data <- merge(geoid, ext, by="fips")
+data <- merge(ref, ext, by="fips")
 
 ## Special Case: Merge on county_fips when ext2 FIPS data has no leading zeros 
 ## (e.g. 1 for Adair County instead of 001)
@@ -50,8 +50,8 @@ data <- merge(geoid, ext, by="fips")
 ## Add a new variable county_fips to ext2 with leading zeros
 ## ext2$county_fips <- str_pad(ext2$FIPS.Code, width=3, pad = "0")
 
-## Merging geoid and ext2 on county_fips
-## data2 <- merge(geoid, ext2, by="county_fips")
+## Merging ref and ext2 on county_fips
+## data2 <- merge(ref, ext2, by="county_fips")
 
 ## Rearranging and deleting unimportant data
 ## data2$bachmann <- data2$Bachmann  
@@ -64,7 +64,7 @@ data <- merge(geoid, ext, by="fips")
 
 ## data2 <- data2[c("id", "county_name", "bachmann", "gingrich", "huntsman", "paul", "perry", "romney", "santorum")]
 ## data2 <- data2[order(data2$id),] # if you have to order the data
-## write.table(data2, file="primary_iowa_county_2012_2.csv", sep="," ,col.names=TRUE, row.names=FALSE)
+## write.table(data2, file="primary_iowa_county_2012_data2.csv", sep="," ,col.names=TRUE, row.names=FALSE)
 
 
 ## head(data)
