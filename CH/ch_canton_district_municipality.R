346921a3f7a3472ca819f5df468805580cc55eae
Roger Fischer <roger.fischer@kaywa.com>
2016-01-18 17:37:47 -0800

    changed file names

--- CH/ch_canton_district_municipality.R
+++ CH/ch_canton_district_municipality.R
@@ -18,15 +18,15 @@ require(gdata)
 
 ## Canton
 canton <- read.xls ("ch_canton_district_municipality_2016.xls", sheet = 4, header = TRUE, stringsAsFactors=FALSE)
-write.table(canton, file="canton/ch_canton_2016.csv", sep="," ,col.names=TRUE, row.names=FALSE)
+write.table(canton, file="canton/ch_canton_2016_source.csv", sep="," ,col.names=TRUE, row.names=FALSE)
 
 ## District
 district <- read.xls ("ch_canton_district_municipality_2016.xls", sheet = 3, header = TRUE, stringsAsFactors=FALSE)
-write.table(district, file="district/ch_district_2016.csv", sep="," ,col.names=TRUE, row.names=FALSE)
+write.table(district, file="district/ch_district_2016_source.csv", sep="," ,col.names=TRUE, row.names=FALSE)
 
 ## Muncipality
 municipality <- read.xls ("ch_canton_district_municipality_2016.xls", sheet = 2, header = TRUE, stringsAsFactors=FALSE)
-write.table(municipality, file="municipality/ch_municipality_2016.csv", sep="," ,col.names=TRUE, row.names=FALSE)
+write.table(municipality, file="municipality/ch_municipality_2016_source.csv", sep="," ,col.names=TRUE, row.names=FALSE)
 
 
 
