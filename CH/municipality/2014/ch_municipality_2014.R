6a8f86ca0e7feacb6a968be3aea8849380576b94
Roger Fischer <roger.fischer@kaywa.com>
2016-01-25 18:12:54 -0800

    new csv

--- CH/municipality/2014/ch_municipality_2014.R
+++ CH/municipality/2014/ch_municipality_2014.R
@@ -11,3 +11,25 @@
 fileUrl <- "http://www.bfs.admin.ch/bfs/portal/de/index/themen/17/03/blank/key/2014/013.Document.176630.xls"
 download.file(fileUrl, destfile = "ext.xls", method = "curl")
 
+dateDownloaded <- date()
+dateDownloaded "Mon Jan 25 17:37:43 2016"
+
+install.packages("gdata")
+require(gdata)
+ext <- read.xls ("ext.xls", sheet = 1, header = TRUE, stringsAsFactors=FALSE)
+
+## without head
+ext_csv1 <- ext[-c(1:5), ]
+## remove first two colums 
+ext_csv2 <- ext_csv1[, -c(1:2)] 
+ext_csv3 <- ext_csv2[-c(2373:2377), ]
+ext_csv4 <- ext_csv3[, -c(10:13) ]
+dim(ext_csv4)
+head(ext_csv4)
+tail(ext_csv4)
+
+write.table(ext_csv4, file="ext_initiative_20140902.csv", sep="," , col.names=TRUE, row.names=FALSE)
+
+
+
+
