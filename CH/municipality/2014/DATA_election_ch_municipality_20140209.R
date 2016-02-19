6dc6b1043619b297279250140b08e91996e4fb10
Roger Fischer <roger.fischer@kaywa.com>
2016-02-18 16:11:26 -0800

    updated R script

--- CH/municipality/2014/DATA_election_ch_municipality_20140209.R
+++ CH/municipality/2014/DATA_election_ch_municipality_20140209.R
@@ -7,10 +7,15 @@
 ref_file <- "ch_municipality_2014_with_swiss_abroad.csv"
 ref <- read.csv(ref_file, header = TRUE, sep = ",", stringsAsFactors=FALSE, fileEncoding="UTF-8") 
 
+## EXT
 ## https://raw.githubusercontent.com/datamapio/geoid/master/CH/municipality/2014/ext1_initiative_20140902.csv  
 ext_file <- "ext1_initiative_20140902.csv"
 ext <- read.csv(ext_file, header = TRUE, sep = ",", stringsAsFactors=FALSE) 
+x <- ext[, c(3:9)] 
+x <- apply(x, 2, function(y) as.numeric(gsub(",", "", y)))
+ext[, c(3:9)] <- x
 
+## Merge REF and EXT
 data <- merge(ref, ext, by="gdenr", all=TRUE)
 library(dplyr)
 ## entitled_to_vote, votes_cast, turnout, valid_votes,   yes, no, yes_percentage
