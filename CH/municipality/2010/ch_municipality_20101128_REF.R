395f512e05c7ede4f0f315c20ca61274b9493446
Roger Fischer <roger.fischer@kaywa.com>
2016-02-22 10:11:32 -0800

    New November File

--- CH/municipality/2010/ch_municipality_20101128_REF.R
+++ CH/municipality/2010/ch_municipality_20101128_REF.R
@@ -17,10 +17,43 @@ ref$id <- paste("756", ref$district_number, ref$municipality_number, sep="")
 
 library(dplyr)
 ref <- select(ref, id, municipality_number, municipality_name, district_number, canton_number, GMDE, BEZIRK, KT, NAME)
+ref_reduced <- ref[(1:2600), ]
 
-write.table(ref, file="ch_municipality_2010_with_kommunanzen.csv", sep="," , col.names=TRUE, row.names=FALSE)
-
+write.table(ref_reduced, file="ch_municipality_2010_with_kommunanzen.csv", sep="," , col.names=TRUE, row.names=FALSE)
 
 ## THE REF FILE FOR THE ELECTION, NOVEMBER 28, 2010
+## Remove these 14
+##---,5011 Fusion with 5238
+##---,5098 Fusion with 5398
+##---,5101 Fusion with 5398
+##---,5106 Fusion with 5398
+##---,5110 Fusion with 5398
+##---,5116 Fusion with 5398
+##---,5123 Fusion with 5398
+##---,5127 Fusion with 5398
+##---,5128 Fusion with 5398
+##---,5134 Fusion with 5398
+##---,5153 Fusion with 5238
+##---,5165 Fusion with 5238
+##---,5217 Fusion with 5238
+##---,5223 Fusion with 5238
+del_muni <- c("5011", "5098", "5101", "5106", "5110", "5116", "5123", "5127", "5128", "5134", "5153", "5165", "5217", "5223")
+ref_nov <-ref_reduced[!(ref_reduced$municipality_number %in% del_muni), ] 
+
+## Add 5238 and 5398
+id <- c(75621055238, 75621045398)
+municipality_number <- c(5238, 5398)
+municipality_name <- c("Monteceneri", "Gambarogno")
+district_number <- c(2105, 2104)
+canton_number <- c(21, 21)
+GMDE <- c(5238, 5398)
+BEZIRK <- c(2105, 2104)
+KT <- c(21, 21)               
+NAME <- c("Monteceneri", "Gambarogno")
+new5238_5398 <- data.frame(id, municipality_number, municipality_name, district_number, canton_number, GMDE, BEZIRK, KT, NAME)
+
+ref_nov <- rbind(ref_nov, new5238_5398)
+ref_nov <- ref_nov[order(ref_nov$id), ] 
+## Check: ref_nov[(1818:1878), ]
 
 
