e6d8ba3f9d3889f5c0b878c709bde8662419627b
Roger Fischer <roger.fischer@kaywa.com>
2016-03-04 15:46:29 -0800

    cleaned R file

--- BE/province/REF_be_province.R
+++ BE/province/REF_be_province.R
@@ -1,14 +1,6 @@
 ## setwd("/Users/rogerfischer/datamap/geoid/BE/province")
 ## getwd()
 
-Type: Alphanumeric, 5
-Country (3, numeric, ISO) | Region Code (1, alphabetic) | Province Code (1, numeric)
-056 | W | 2
-Ex. 056W2 = Brabant wallon
-Ex. 056F2 = Vlaams-Brabant
-Ex. 056F4 = Flandres Orientale
-Note: We use the alphabet to keep the link to the province numbers.
-
 ref <- read.csv("RAW_belgium_province_2013.csv", header = TRUE, stringsAsFactors=FALSE, encoding = "UTF-8")
 
 ref$id <- c("056F1", "056F2", "056W2", "056F3", "056F4", "056W5", "056W6", "056F7", "056W8", "056W9")
