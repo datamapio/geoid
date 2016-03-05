fceeaeb2cde9792100a2005775d6f497d9f1d6ac
Roger Fischer <roger.fischer@kaywa.com>
2016-03-04 17:42:56 -0800

    upload of unfinished R script

--- BE/arrondissement/REF_be_arrondissement.R
+++ BE/arrondissement/REF_be_arrondissement.R
@@ -1,19 +1,35 @@
 ## setwd("/Users/rogerfischer/datamap/geoid/BE/arrondissement")
 ## getwd()
 
-ref <- read.csv("RAW_belgium_province_2013.csv", header = TRUE, stringsAsFactors=FALSE, encoding = "UTF-8")
+ref <- read.csv("RAW_belgium_arrondissement_2013.csv", header = TRUE, stringsAsFactors=FALSE, encoding = "UTF-8")
+ref2 <- read.csv("be_arrondissement_wikipedia.csv", header = TRUE, stringsAsFactors=FALSE, encoding = "UTF-8")
+
+names(ref) <- c("arrondissement_name_fr", "code_nis_5")
+##trim leading and trailing zeros function
+trim <- function (x) gsub("0{3}$", "", x)
+
+## apply trim function
+ref$code_nis <- trim(ref$code_nis_5)
+
+##Merge
+ref3 <- merge(ref, ref2, by="code_nis")
+library(dplyr)
+ref3 <- select(ref3, code_nis, arr_name_nl, arr_name_fr, arrondissement_name_fr, code_hasc, code_nuts, code_nis_5)
+write.table(ref3, "RAW_MERGE_be_arr_2013.csv", sep="," , col.names=TRUE, row.names=FALSE)
+
+
+## TO DO FROM DOWN HERE
+#fla <- c(11, 12, 13, 23, 24, 31, 32, 33, 34, 35, 36, 37, 38, 41, 42, 43, 44, 45, 46, 71, 72, 73)
+#
+#
+#createid <- function (x) ref3$id[ref3$code_nis == x] <- paste("056F", ref3$code_nis, sep="")
+#for(i in fla) {
+#  createid(i)
+#}
+
+
 
 ref$id <- c("056F1", "056F2", "056W2", "056F3", "056F4", "056W5", "056W6", "056F7", "056W8", "056W9")
-names(ref) <- c("province_name_fr", "code_ins", "id")
-
 ref$province_name <- c("Antwerp", "Flemish Brabant", "Walloon Brabant", "West Flanders", "East Flanders", "Hainaut", "Liège", "Limburg", "Luxembourg", "Namur")
-ref$province_name_nl <- c("Antwerpen", "Vlaams-Brabant", "Waals-Brabant", "West-Vlaanderen", "Oost-Vlaanderen", "Henegouwen", "Luik", "Limburg", "Luxemburg", "Namen")
-ref$province_name_fr <- c("Anvers", "Brabant Flamand", "Brabant Wallon", "Flandre Occidentale", "Flandre Orientale", "Hainaut", "Liège", "Limbourg", "Luxembourg", "Namur")
-ref$province_name_de <- c("Antwerpen", "Flämisch-Brabant", "Wallonisch-Brabant", "Westflandern", "Ostflandern", "Hennegau", "Lüttich", "Limburg", "Luxemburg", "Namür")
-
-library(dplyr)
-ref <- select(ref, id, province_name, province_name_fr, province_name_nl, province_name_de, code_ins)
-
-write.table(ref, file="be_province_2016.csv", sep="," ,col.names=TRUE, row.names=FALSE)
 
 
