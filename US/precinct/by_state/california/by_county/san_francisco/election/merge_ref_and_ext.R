10a9d70db41afc8966a4eb2987d81e28ebabccf8
Roger Fischer <roger.fischer@kaywa.com>
2016-04-12 13:03:48 -0700

    changed to yes-percentage

--- US/precinct/by_state/california/by_county/san_francisco/election/merge_ref_and_ext.R
+++ US/precinct/by_state/california/by_county/san_francisco/election/merge_ref_and_ext.R
@@ -13,5 +13,5 @@ ext <- read.csv("ext/prop_f_election_day_20151103_20151119.csv", header = TRUE, 
 data <- merge(ref, ext, by.x="pct_2012", by.y="polling_precinct", all=TRUE)
 
 library(dplyr)
-data <- select(data, id, pct_2012, precinct_names, neighborhood, no_percentage)
+data <- select(data, id, pct_2012, precinct_names, neighborhood, yes_percentage)
 write.table(data, file="DATA_prop_f_election_day_no_percentage_20151103_20151119.csv", sep="," ,col.names=TRUE, row.names=FALSE)
